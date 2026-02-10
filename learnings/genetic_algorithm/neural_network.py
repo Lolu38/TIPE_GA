3f95c6f99c6930b4d005fd35d58cc23c8b317bf0
Lolu38 <chiappetta.lucas38@gmail.com>
2026-02-10 18:04:54 +0100

    Adding the first part of GPU

---
+++ learnings/genetic_algorithm/neural_network.py
@@ -0,0 +1,15 @@
+import torch
+
+class NeuraAgent ():
+
+    def forward(): # Effectue l'action demandé
+        pass
+
+    def _get_genome(): # Récupère le génome d'un individu
+        pass
+
+    def _set_genome(): # Créé un individu avec un génome particulier
+        pass
+
+    def _mutate_genome(): # Modifie le génome d'un individu
+        pass
---
+++ learnings/genetic_algorithm/pop_manager.py
@@ -0,0 +1,15 @@
+import torch
+
+class PopulationManager():
+
+    def select_best(): # Sélectionne les n% meilleurs agents
+        pass
+
+    def crossover(): # reproduit deux parents entre eux
+        pass
+
+    def reproduce(): # création d'une nouvelle génération entière
+        pass
+
+    def _get_actions(): # permet de récupérer les actions à effectuer
+        pass
